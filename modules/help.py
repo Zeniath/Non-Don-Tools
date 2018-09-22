eb8e69dc9f3aab54effb613b6931c5bf63d261fe
Zeniath <harveycrennison@gmail.com>
2018-09-22 16:23:33 +1200

    added a ``source`` command!!

--- modules/help.py
+++ modules/help.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from utils.HelpPaginator import HelpPaginator, CannotPaginate
 from datetime import datetime
 import datetime
+import inspect, os
 
 class Help:
     """Helpful Commands"""
@@ -34,7 +35,14 @@ class Help:
     @commands.command()
     @commands.has_permissions(manage_messages=True)
     async def purge(self, ctx, number: int, target: discord.User = None):
-        """Purges any amount of messages"""
+        """Purges any amount of messages
+
+        If a user is provided, it will clear only their messages
+
+        If no user is provided, it will clear all of the messages
+
+        You must have Manage Messages permissions to use this command"""
+
         if number <= 0:
             em = discord.Embed(color=16720640)
             em.add_field(name="Error <:no:473312284148498442>", value=f"You must purge at least 1 message!")
@@ -52,6 +60,29 @@ class Help:
         embed = discord.Embed(title='Purged <:yes:473312268998803466>', description='\n'.join(messages), color=9305953)
         embed.set_footer(text=f'Requested by {ctx.author}', icon_url=ctx.author.avatar_url)
         await ctx.send(embed=embed, delete_after=5)
+
+    @commands.command(aliases=['sauce', 'sawce'])
+    async def source(self, ctx, command: str = None):
+        """Get the bot's source link for a command or the whole source"""
+
+        source_url = "https://github.com/Zeniath/Non-Don-Tools"
+        if command is None:
+            return await ctx.send(source_url)
+        
+
+        obj = self.bot.get_command(command.replace('.', ' '))
+        if obj is None:
+            return await ctx.send('Could not find command.')
+
+        src = obj.callback.__code__
+        lines, firstlineno = inspect.getsourcelines(src)
+        if not obj.callback.__module__.startswith('discord'):
+            location = os.path.relpath(src.co_filename).replace('\\', '/')
+        else:
+            location = obj.callback.__module__.replace('.', '/') + '.py'
+            source_url = "https://github.com/Zeniath/Non-Don-Tools"
+
+        await ctx.send(f"<{source_url}/tree/master/{location}/#L{firstlineno}-L{firstlineno + len(lines) - 1}>")
 
     @commands.command(aliases=['feedback', 'fb', 'bug', 'suggest'])
     async def report(self, ctx, *, suggestion: str = None):
