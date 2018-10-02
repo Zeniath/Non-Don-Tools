c03e865a2e2412a9afc63d65f6c330b912054c2a
Zeniath <harveycrennison@gmail.com>
2018-10-02 22:34:54 +1300

    changes in a few files

--- modules/utils/hastebin.py
+++
@@ -1,7 +0,0 @@
-import aiohttp
-
-async def post(content):
-    async with aiohttp.ClientSession() as session:
-        async with session.post("https://hastebin.com/documents",data=content.encode('utf-8')) as post:
-            post = await post.json()
-            return "https://hastebin.com/{}".format(post['key'])
